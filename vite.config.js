b03eb6532ad5502283fcbf75c358abae3f2d5949
zephyrhealthcare <163467511+zephyrhealthcare@users.noreply.github.com>
2024-03-28 02:42:49 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
 import path from "path";
 import {defineConfig} from "vite";
 
-export default {
+export default defineConfig({
+  // root: "./test/plots",
   base: '/plot/'
-}
-
-// export default defineConfig({
-//   root: "./test/plots",
-//   base: "/plot/",
-//   publicDir: path.resolve("./test"),
-//   resolve: {
-//     alias: {
-//       "@observablehq/plot": path.resolve("./src/index.js")
-//     }
-//   },
-//   server: {
-//     port: 8008,
-//     open: "/"
-//   }
-// });
+  // publicDir: path.resolve("./test"),
+  // resolve: {
+  //   alias: {
+  //     "@observablehq/plot": path.resolve("./src/index.js")
+  //   }
+  // },
+  // server: {
+  //   port: 8008,
+  //   open: "/"
+  // }
+});
